1c8ecb1aab78bf86f5c8e405ddb5f7bf866d6470
ashik029 <mrashik029@gmail.com>
2022-02-14 17:31:44 +0600

    Day22

--- pages/productDetails.php
+++ pages/productDetails.php
@@ -10,7 +10,7 @@
                       <h2 class="font-weight-bold"><?php echo $productDetails['name']; ?></h2>
                     <p>Price: BDT <?php echo $productDetails['price']; ?></p>
                     <p><?php echo $productDetails['description']; ?></p>
-                    <a href="" class="btn btn-outline-success">Order Now</a>
+                    <a href="javascript:void(0)" class="btn btn-outline-success">Order Now</a>
                 </div>
                 </div>
             </div>
